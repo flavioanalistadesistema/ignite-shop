4196bf49f5c6fda6f065f2b5a9d0ec5ff2738b06
Flavio Santos <flavioanalistadesistema@gmail.com>
2023-08-19 15:53:01 -0300

    feat: Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,29 @@
+import { styled } from "../styleds"
+
+const Button = styled('button', {
+  backgroundColor: '$primary',
+  border: 0,
+  padding: '4px 8px',
+  borderRadius: 4,
+
+  span: {
+    fontWeight: 700,
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.7)',
+  }
+
+})
+
 
 export default function Home() {
   return (
       <div>
-        <h1>Hello word</h1>
+        <Button>
+          <span>Teste </span>
+          Hello word
+        </Button>
       </div>
   )
 }
